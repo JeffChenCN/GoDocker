df0ea0e3f8bceec4be9f29399b0f8458f94afafc
jeffcng@gmail.com <jeffcng@gmail.com>
2020-04-01 11:23:12 -0700

    more init

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html"
 	"log"
 	"net/http"
 )
@@ -12,6 +13,10 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Go running in Docker Container")
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi Gopher Docker %q", html.EscapeString(r.URL.Path))
 	})
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
